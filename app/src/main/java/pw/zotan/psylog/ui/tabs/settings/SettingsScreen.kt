a015124234744efbf22e743c97a1f596eeb11a53
Folf <folf@folf.me>
2025-09-07 21:23:20 +0200

    update license

--- app/src/main/java/pw/zotan/psylog/ui/tabs/settings/SettingsScreen.kt
+++ app/src/main/java/pw/zotan/psylog/ui/tabs/settings/SettingsScreen.kt
@@ -1,21 +1,3 @@
-/*
- * Copyright (c) 2022-2023. Isaak Hanimann.
- * This file is part of PsychonautWiki Journal.
- *
- * PsychonautWiki Journal is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or (at
- * your option) any later version.
- *
- * PsychonautWiki Journal is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with PsychonautWiki Journal.  If not, see https://www.gnu.org/licenses/gpl-3.0.en.html.
- */
-
 package pw.zotan.psylog.ui.tabs.settings
 
 import android.content.Intent
@@ -445,7 +427,7 @@ fun SettingsScreen(
             }
             CardWithTitle(title = "App", innerPaddingHorizontal = 0.dp) {
                 SettingsButton(imageVector = Icons.Outlined.Code, text = "Source Code") {
-                    uriHandler.openUri("https://github.com/isaakhanimann/psychonautwiki-journal-android")
+                    uriHandler.openUri("https://git.ztn.sh/PsyLog/PsyLog-android")
                 }
                 HorizontalDivider()
                 val context = LocalContext.current
